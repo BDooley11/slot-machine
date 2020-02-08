a809cff8f2483547503928c0246c73417b61928e
BDooley11 <55091575+BDooley11@users.noreply.github.com>
2020-02-08 18:12:29 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+requirements = [emoji,random]
+
+setup(name="slotMachine",
+      version="0.1",
+      description="Basic slot machine",
+      url="",
+      author="Barry Dooley",
+      author_email="barry.dooley@ucd.ie",
+      licence="GPL3",
+      packages=['slotMachine'],
+      install_requires=requirements,
+      entry_points={
+        'console_scripts':['run slotMachine=slotMachine.main:main']
+        }
+      )
